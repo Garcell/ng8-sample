32e33cb00cc534d93751afc9f61d32a295620504
Nikolas LeBlanc <nikolas.leblanc@gmail.com>
2023-03-29 16:39:47 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'ng8';
+  title = "ng8";
+  myScriptElement;
+
+  script = `iframe = document.querySelector(
+        "iframe[data-dvc-widget='dvc-iframe']"
+      );
+      window.addEventListener(
+        "message",
+        function (e) {
+          let t = e.data;
+          "DVC.optIn.updateHeight" === t.type && (iframe.style.height = t.height);
+        },
+        !1
+      );
+      `;
+
+  ngOnInit() {
+    this.myScriptElement = document.createElement("script");
+    this.myScriptElement.text = this.script;
+    document.body.appendChild(this.myScriptElement);
+    console.log(this.myScriptElement);
+  }
 }
